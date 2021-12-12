b9216d6c99a64e7cbb669ad599f15635f662f8b2
Venki4Github <venkatesh.vs9264@gmail.com>
2021-12-12 22:42:16 +0530

    Modified the file

--- Sample.java
+++ Sample.java
@@ -5,6 +5,10 @@ class Sample
 		System.out.println ("welcome to the Git");
 		System.out.println ("welcome to the Git");
 		System.out.println ("welcome to the Git");
+		int a = 10;
+		int b = 20;
+		c = a + b;
+		System.out.println ("Sub of a & b" +c);
 	}
 }
 
